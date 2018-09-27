e091b9187ebf5f9e24d47e20895422ab8a280175
JoseFerrer <jcfb90@gmail.com>
2018-09-27 15:27:02 -0500

    Start to NewDashBoardFront

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React, {Component} from "react";
+
+class App extends Component {
+    render(){
+        return(
+            <div>  Hola estoy proabdo el react con webpack  </div>
+        );
+    }
+}
+export default App;
